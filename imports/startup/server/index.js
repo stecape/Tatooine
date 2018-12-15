0b23f84345e4b8f578871c736a0f3f0353c8b8b8
Stefano <ste.cape@gmail.com>
2018-12-15 18:12:34 +0100

    Implemented OWN Communication Motor

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor'
 import '../../api/users.js'
 import '../../api/googleApiCalls.js'
-import { ServiceConfiguration } from 'meteor/service-configuration';
+import { ServiceConfiguration } from 'meteor/service-configuration'
 
 Meteor.startup(() => {
 
@@ -22,4 +22,26 @@ Meteor.startup(() => {
 	var noAdminsYet = Meteor.users.find({roles: {  $in: ['ras'] }}).count() == 0
 	var users = Meteor.users.find({}, { sort: { createdAt: 1 } } )
 	noAdminsYet && users.count() > 0 && Roles.setUserRoles(users.fetch()[0]._id, ['ras'])
+
+	// creating OWN connection to the F454
+  var myhome = require('myhome')
+  var mhengine = new myhome.engine({ 
+    "host": "192.168.0.35",
+    "port": "20000",
+    "pass": "12345"
+  })
+
+  //When the "monitoring" is emitted, log it
+  mhengine.on ('monitoring', function () { 
+    console.log ('successfully connected, started monitoring') 
+  })
+
+  //how to intercept a packet:
+	mhengine.on ('packet', function (pkt) {
+    if (pkt == "*1*0*23##") console.log ('Luce 23 OFF') 
+  })
+
+  //if you want to send a command, do like this
+  //mhengine.sendCommand({command: "*1*0*23##"})
+
 })
--- imports/ui/pages/Home.js
+++ imports/ui/pages/Home.js
@@ -6,19 +6,11 @@ export default class Home extends React.Component {
 
   render () {
 
-    //call to the google contacts API, for fetching the contacts of a user. 1 - Retrieve the access token from the server, 2 - Async call to google contacts api, 3 - Callback that logs the result or the error.
-  	this.getContacts = () => {
-      Meteor.call('fetchAcessToken', (error, accessToken) => { 
-          return axios.get('https://www.google.com/m8/feeds/contacts/default/full?access_token=' + accessToken + "&alt=json&max-results=2000")
-          .then( function (resp) {console.log(resp.data.feed.entry)} )
-          .catch( function (error) {console.log(error)} )
-      })
-    }
   	return(
   		<div>
         {/*This render is conditioned by the role of the user: the link to the users management section is rendered only if the user is a 'ras'*/}
   			{ Roles.userIsInRole(this.props.user, ['ras']) && <Link to="/admin">Admin</Link> }
-  			<button onClick={this.getContacts}>Click</button>
+  			<button>Click</button>
   		</div>
   	)
   }
