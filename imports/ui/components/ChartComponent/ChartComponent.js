4cecf14c4bc931b2c18384dac9ef61bbaabe0bc2
stecape <ste.cape@gmail.com>
2019-02-28 11:22:41 +0100

    Real Time Chart

--- imports/ui/components/ChartComponent/ChartComponent.js
+++ imports/ui/components/ChartComponent/ChartComponent.js
@@ -160,8 +160,8 @@ export default HighChartsContainer = withTracker((props) => {
   Meteor.subscribe('histTemperaturesData')
   Meteor.subscribe('histMeteoData')
   return {
-    histTemperatures: HistTemperatures.find({ts: {$gte: new Date(new Date().setDate(new Date().getDate()-1)), $lte: new Date()}}).fetch(),
-    histMeteo: HistMeteo.find({ts: {$gte: new Date(new Date().setDate(new Date().getDate()-1)), $lte: new Date()}}).fetch(),
+    histTemperatures: HistTemperatures.find({ts: {$gte: new Date(new Date().setDate(new Date().getDate()-1))}}).fetch(),
+    histMeteo: HistMeteo.find({ts: {$gte: new Date(new Date().setDate(new Date().getDate()-1))}}).fetch(),
     detail: "highest"
   }
 })(ChartComponentContainer)
